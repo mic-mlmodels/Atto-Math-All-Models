86207686682bab229bd3a151dfe8e89d33164f2b
Michael <michaelzyx1@gmail.com>
2026-07-07 19:59:47 +1000

    lora --> qlora upgrade

--- lora.py
+++ lora.py
@@ -1,20 +1,27 @@
+from torch import bfloat16, uint8
 import torch.nn as nn
 from transformers import Conv1D
+import bitsandbytes as bnb
+from bitsandbytes.nn.modules import Linear4bit
 
 
 class LayerAdaptor(nn.Module):
     def __init__(self, original_layer, bottleneck_rank, layer_type):
         super().__init__()
-        self.original_layer = original_layer
+        self.original_layer = Linear4bit(
+            original_layer.in_features,
+            original_layer.out_features,
+            bias=False,
+            compute_dtype=bfloat16,
+            quant_type="nf4",
+            quant_storage=uint8,
+        )
+        for param in self.original_layer.parameters():
+            param.requires_grad = False
         if layer_type == "linear":
             self.adaptor = nn.Sequential(
                 nn.Linear(self.original_layer.in_features, bottleneck_rank),
                 nn.Linear(bottleneck_rank, self.original_layer.out_features),
-            )
-        elif layer_type == "conv":
-            self.adaptor = nn.Sequential(
-                nn.Linear(self.original_layer.weight.shape[0], bottleneck_rank),
-                nn.Linear(bottleneck_rank, self.original_layer.weight.shape[1]),
             )
 
     def forward(self, x):
@@ -27,7 +34,5 @@ def adapt_model(model, bottleneck_rank):
             continue
         if isinstance(layer, nn.Linear):
             setattr(model, layer_name, LayerAdaptor(layer, bottleneck_rank, "linear"))
-        elif isinstance(layer, Conv1D):
-            setattr(model, layer_name, LayerAdaptor(layer, bottleneck_rank, "conv"))
         else:
             adapt_model(layer, bottleneck_rank)
