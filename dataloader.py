9483da8b7bd6d5bbd48d8e3880fb019080ef91af
Michael <michaelzyx1@gmail.com>
2026-07-08 13:50:23 +1000

    finished batched dataloader but shape mismatch error, original setup code suspected

--- dataloader.py
+++ dataloader.py
@@ -22,34 +22,23 @@ class Dataloader:
             )
 
     def extract(self, entries):
+        input_ids_lst = [entry["input_ids"] for entry in entries]
+        attention_mask_lst = [entry["attention_mask"] for entry in entries]
+        labels_lst = [entry["labels"] for entry in entries]
+
+        max_length = max([len(tokens) for tokens in input_ids_lst])
         padded_text_tokens_lst = []
         masks_lst = []
         label_lst = []
-        new_padded_text_tokens_lst = []
-        new_masks_lst = []
-        new_label_lst = []
-        for entry in entries:
-            padded_text_tokens_lst.append(entry["input_ids"])
-            masks_lst.append(entry["attention_mask"])
-            label_lst.append(entry["labels"])
-        max_length = max([len(tokens) for tokens in padded_text_tokens_lst])
-        for padded_text_tokens in padded_text_tokens_lst:
-            padding = max_length - len(padded_text_tokens)
-            new_padded_text_tokens_lst.append(
-                padded_text_tokens + [self.tokeniser.eos_token_id] * padding
+        for input_ids, attention_mask, labels in zip(
+            input_ids_lst, attention_mask_lst, labels_lst
+        ):
+            padding = max_length - len(input_ids)
+            padded_text_tokens_lst.append(
+                input_ids + [self.tokeniser.eos_token_id] * padding
             )
-        for padded_text_tokens in padded_text_tokens_lst:
-            padding = max_length - len(padded_text_tokens)
-            new_padded_text_tokens_lst.append(
-                padded_text_tokens + [self.tokeniser.eos_token_id] * padding
-            )
-
-        for padded_text_tokens in padded_text_tokens_lst:
-            padding = max_length - len(padded_text_tokens)
-            new_padded_text_tokens_lst.append(
-                padded_text_tokens + [self.tokeniser.eos_token_id] * padding
-            )
-
+            masks_lst.append(attention_mask + [0] * padding)
+            label_lst.append(labels + [-100] * padding)
         return {
             "input_ids": torch.tensor(padded_text_tokens_lst),  # type: ignore
             "attention_mask": torch.tensor(masks_lst),
