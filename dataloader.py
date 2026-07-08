599434c4bfdd589ee873215e5c79e540f5777b3e
Michael <michaelzyx1@gmail.com>
2026-07-08 13:31:31 +1000

    training loop done and tried to batch dataloader (horrifically unoptimised and uncomplete)

--- dataloader.py
+++ dataloader.py
@@ -25,10 +25,31 @@ class Dataloader:
         padded_text_tokens_lst = []
         masks_lst = []
         label_lst = []
+        new_padded_text_tokens_lst = []
+        new_masks_lst = []
+        new_label_lst = []
         for entry in entries:
             padded_text_tokens_lst.append(entry["input_ids"])
             masks_lst.append(entry["attention_mask"])
             label_lst.append(entry["labels"])
+        max_length = max([len(tokens) for tokens in padded_text_tokens_lst])
+        for padded_text_tokens in padded_text_tokens_lst:
+            padding = max_length - len(padded_text_tokens)
+            new_padded_text_tokens_lst.append(
+                padded_text_tokens + [self.tokeniser.eos_token_id] * padding
+            )
+        for padded_text_tokens in padded_text_tokens_lst:
+            padding = max_length - len(padded_text_tokens)
+            new_padded_text_tokens_lst.append(
+                padded_text_tokens + [self.tokeniser.eos_token_id] * padding
+            )
+
+        for padded_text_tokens in padded_text_tokens_lst:
+            padding = max_length - len(padded_text_tokens)
+            new_padded_text_tokens_lst.append(
+                padded_text_tokens + [self.tokeniser.eos_token_id] * padding
+            )
+
         return {
             "input_ids": torch.tensor(padded_text_tokens_lst),  # type: ignore
             "attention_mask": torch.tensor(masks_lst),
--- sft_train.py
+++ sft_train.py
@@ -1,11 +1,12 @@
 # %%
 # imports
 from random import shuffle
-
+import numpy as np
 import torch
 from datasets import load_from_disk, Dataset
 from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer
 from dataloader import Dataloader
+import dataloader
 from qlora import adapt_model
 
 data = load_from_disk("processed-metamathqa")
@@ -19,14 +20,67 @@ val_data = data["test"]
 BATCH_SIZE = 4
 BOTTNECK_RANK = 4
 LORA_ALPHA = BOTTNECK_RANK * 2
+LR = 3e-4
+NUM_STEPS = 100
 device = "cuda" if torch.cuda.is_available() else "cpu"
 tokeniser = AutoTokenizer.from_pretrained("Qwen2.5-1.5B base model")
 model = AutoModelForCausalLM.from_pretrained("Qwen2.5-1.5B base model")
-train_dataloader = Dataloader(
-    dataset=train_data, shuffle=True, tokeniser=tokeniser, batch_size=BATCH_SIZE
-)
+train_dataloader = Dataloader(train_data, True, tokeniser, BATCH_SIZE)
+val_dataloader = Dataloader(val_data, False, tokeniser, BATCH_SIZE)
+train_iter = iter(train_dataloader)
+val_iter = iter(val_dataloader)
 adapt_model(model, BOTTNECK_RANK, device, LORA_ALPHA)
 model.to(device)  # type: ignore
+optimiser = torch.optim.AdamW(
+    params=[param for param in model.parameters() if param.requires_grad], lr=LR
+)
 
 # %%
 # training yippee :D
+print("Atto-Math-SFT model cooking...")
+train_loss_lst = []
+mean_train_loss_lst = []
+val_loss_lst = []
+mean_val_loss_lst = []
+for step in range(NUM_STEPS):
+    model.train()
+    try:
+        param_dict = next(train_iter)
+    except StopIteration:
+        train_iter = iter(train_dataloader)
+        param_dict = next(train_iter)
+    param_dict = {k: v.to(device) for k, v in param_dict.items()}
+    out = model(**param_dict)
+    loss = out.loss
+    loss.backward()
+    optimiser.step()
+    optimiser.zero_grad()
+    train_loss_lst.append(loss.item())
+    if step % 20 == 0:
+        train_loss_mean = np.mean(train_loss_lst)
+        mean_train_loss_lst.append(train_loss_mean)
+        train_loss_lst = []
+        model.eval()
+        with torch.no_grad():
+            try:
+                val_param_dict = next(val_iter)
+            except StopIteration:
+                val_iter = iter(val_dataloader)
+                val_param_dict = next(val_iter)
+            val_param_dict = {k: v.to(device) for k, v in val_param_dict.items()}
+            val_out = model(**val_param_dict)
+            val_loss = val_out.loss
+            val_loss_lst.append(val_loss.item())
+        print(f"train loss: {train_loss_mean}")
+        del val_loss, val_out, val_param_dict
+    if step % 100:
+        val_loss_mean = np.mean(val_loss_lst)
+        mean_val_loss_lst.append(val_loss_mean)
+        val_loss_lst = []
+        print(f"val loss: {val_loss_mean}")
+
+    del out, loss, param_dict
+print("Atto-Math-SFT model cooked!")
+
+# %%
+# eval time
