acbadcf2a20cd32e12a552f055a23dafc039d778
Michael <michaelzyx1@gmail.com>
2026-07-15 21:14:48 +1000

    saved only trained lora adaptor params so changed torch.load to just pass the params i have

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def load_cooked_model(BOTTNECK_RANK, LORA_ALPHA, device, params_path):
     adapt_model(model, BOTTNECK_RANK, LORA_ALPHA)
     model.to(device)  # type: ignore
     model.load_state_dict(
-        torch.load(params_path, weights_only=False)["model_state_dict"]
+        torch.load(params_path, weights_only=False)["model_state_dict"], strict=False
     )  # for anyone running this code (thx for checking it out btw), i set the weights_only param as False but this is safe as its just primarily to allow some numpy ops in the pickle stream and nothing malicious
 
 
