33ea14df58710cc6f45250360970bdedbb54904c
Michael <michaelzyx1@gmail.com>
2026-07-20 15:57:49 +1000

    fixed up grpo code but still need more tweaking, still suspect many errors in code especially shape mismatches

--- grpo_train.py
+++ grpo_train.py
@@ -14,7 +14,7 @@ import bitsandbytes as bnb
 # setup
 
 MAX_TOKENS = 768
-EVAL_MAJ_BATCH_SIZE = 8
+GROUP_SIZE = 8
 BATCH_SIZE = 16
 BOTTNECK_RANK = 16
 LORA_ALPHA = BOTTNECK_RANK * 2
@@ -24,9 +24,7 @@ MIN_LR = 1e-5
 device = "cuda" if torch.cuda.is_available() else "cpu"
 cwd = os.getcwd()
 data = load_from_disk("processed-metamathqa")
-data = data.filter(
-    lambda x: len(x["input_ids"]) <= MAX_TOKENS
-)  # maybe should have put this in the initial data processing setup but this keeps it more flexible if i need to change filtering for grpo for example
+data = data.filter(lambda x: len(x["input_ids"]) <= MAX_TOKENS)
 data = data.train_test_split(test_size=0.1, train_size=0.9)  # type: ignore
 train_data = data["train"]
 val_data = data["test"]
@@ -52,12 +50,14 @@ optimiser = bnb.optim.PagedAdamW8bit(  # type: ignore
 # grpo time fellas ;D
 
 EPISODE_NUM = 1000
-DISCOUNT = 0.99
 EPSILON = 0.2
 OLD_POLICY_LOOPS = 4
 NEW_POLICY_LOOPS = 8
 new_policy_v0 = model.to(device)  # type: ignore
 old_policy_v0 = model.to(device)  # type: ignore
+original_policy_v0 = model.to(device)  # type: ignore
+for param in original_policy_v0.parameters():
+    param.requires_grad = False
 policy_optimiser = torch.optim.AdamW(lr=3e-4, params=new_policy_v0.parameters())
 episode_rewards = []
 mean_rewards = []
@@ -70,6 +70,8 @@ for episode in range(EPISODE_NUM):
     old_log_probs_stack = []
     old_returns_stack = []
     tokenised_prompt_stack = []
+    original_tokenised_prompt_stack = []
+    combined_mask_stack = []
     with torch.no_grad():
         for i in range(OLD_POLICY_LOOPS):
             print(i)
@@ -77,23 +79,27 @@ for episode in range(EPISODE_NUM):
             current_batch = original_param_dict["input_ids"].shape[0]
             mask = (
                 original_param_dict["attention_mask"]
-                .repeat_interleave(EVAL_MAJ_BATCH_SIZE, dim=0)
+                .repeat_interleave(GROUP_SIZE, dim=0)
                 .to(device)
             )
             input = (
                 original_param_dict["input_ids"]
-                .repeat_interleave(EVAL_MAJ_BATCH_SIZE, dim=0)
+                .repeat_interleave(GROUP_SIZE, dim=0)
                 .to(device),
                 mask,
             )
             tokenised_prompt = (
                 original_param_dict["input_ids"]
-                .repeat_interleave(EVAL_MAJ_BATCH_SIZE, dim=0)
+                .repeat_interleave(GROUP_SIZE, dim=0)
                 .to(device)
             )
+            original_tokenised_prompt_stack.append(tokenised_prompt)
             finished = torch.zeros(
-                current_batch * EVAL_MAJ_BATCH_SIZE,
+                current_batch * GROUP_SIZE,
                 dtype=torch.bool,
+            ).to(device)
+            finished_idx = torch.full(
+                (current_batch * GROUP_SIZE,), -1, dtype=torch.long
             ).to(device)
             imend_token = tokeniser.convert_tokens_to_ids("<|im_end|>")
             kv_cache = None
@@ -111,32 +117,36 @@ for episode in range(EPISODE_NUM):
                 mask = torch.cat(
                     (
                         mask,
-                        torch.ones(
-                            EVAL_MAJ_BATCH_SIZE * current_batch, 1, device=device
-                        ),
+                        torch.ones(GROUP_SIZE * current_batch, 1, device=device),
                     ),
                     dim=-1,
                 )
                 input = (next_word.unsqueeze(1), mask)
-                finished = (
-                    finished
-                    | (next_word == tokeniser.eos_token_id)
-                    | (next_word == imend_token)
+                just_finished = (~finished) & (
+                    (next_word == tokeniser.eos_token_id) | (next_word == imend_token)
                 )
+                finished = finished | just_finished
                 tokenised_prompt = torch.cat(
                     (tokenised_prompt, torch.unsqueeze(next_word, dim=1)),
                     dim=-1,
                 )
+                finished_idx = torch.where(
+                    just_finished,
+                    torch.full_like(finished_idx, tokenised_prompt.shape[-1]),
+                    finished_idx,
+                )
+            finished_idx = torch.where(
+                finished_idx == -1,
+                torch.full_like(finished_idx, tokenised_prompt.shape[-1]),
+                finished_idx,
+            )
             decoded_out = tokeniser.batch_decode(tokenised_prompt)
             old_log_probs_tensor = torch.stack(old_log_probs_lst)
             old_log_probs_stack.append(old_log_probs_tensor)
             for i in range(current_batch):
                 group_correct = 0
                 maj_dict = {}
-                for row in decoded_out[
-                    i * EVAL_MAJ_BATCH_SIZE : i * EVAL_MAJ_BATCH_SIZE
-                    + EVAL_MAJ_BATCH_SIZE
-                ]:
+                for row in decoded_out[i * GROUP_SIZE : i * GROUP_SIZE + GROUP_SIZE]:
                     try:
                         if float(extract_answer(row).replace(",", "")) == float(
                             original_param_dict["labels"][i].replace(",", "")  # type: ignore
@@ -144,6 +154,7 @@ for episode in range(EPISODE_NUM):
                             old_returns_tensor = torch.ones_like(
                                 old_log_probs_tensor, device=device
                             )
+                            group_correct += 1
                         else:
                             old_returns_tensor = torch.zeros_like(
                                 old_log_probs_tensor, device=device
@@ -153,6 +164,15 @@ for episode in range(EPISODE_NUM):
                         old_returns_tensor = torch.zeros_like(
                             old_log_probs_tensor, device=device
                         )
+                episode_rewards.append(group_correct)
+            original_prompt_idx = original_tokenised_prompt_stack[-1].shape[-1]
+            seq_len = tokenised_prompt.shape[-1]
+            positions = torch.arange(1, seq_len, device=device).unsqueeze(0)
+            combined_mask = (
+                (positions >= original_prompt_idx)
+                & (positions <= finished_idx.unsqueeze(1))
+            ).float()
+            combined_mask_stack.append(combined_mask)
             tokenised_prompt_stack.append(tokenised_prompt)
             old_returns_stack.append(old_returns_tensor)  # type: ignore
             del out, original_param_dict, kv_cache  # type: ignore
@@ -161,7 +181,8 @@ for episode in range(EPISODE_NUM):
     )  # type: ignore
     old_log_probs_stack = torch.stack(old_log_probs_stack)
     tokenised_prompt_stack = torch.stack(tokenised_prompt_stack)
-    # UP TO HERE BTW JUST START IMPLEMENTING NEW POLICY LOOP
+    combined_mask_stack = torch.stack(combined_mask_stack)
+    original_tokenised_prompt_stack = torch.stack(original_tokenised_prompt_stack)
     for i in range(NEW_POLICY_LOOPS):
         kv_cache = None
         out = new_policy_v0(
@@ -172,7 +193,15 @@ for episode in range(EPISODE_NUM):
         log_probs = F.log_softmax(logits, dim=-1)
         targets = tokenised_prompt_stack[:, :, 1:]
         new_log_probs = log_probs.gather(-1, targets)
+
+        original_out = original_policy_v0(
+            input_ids=tokenised_prompt_stack, past_key_values=kv_cache, use_cache=True
+        )
+        original_logits = original_out.logits
+        original_log_probs = F.log_softmax(original_logits, dim=-1)
+        original_log_probs = original_log_probs.gather(-1, targets)
         policy_optimiser.zero_grad()
+        old_advantage_tensor *= combined_mask_stack  # breaks computation graph btw but its ok cos we wont backprop thru to old model
         policy_loss = -torch.mean(
             torch.minimum(
                 torch.exp(new_log_probs - old_log_probs_stack) * old_advantage_tensor,
@@ -183,6 +212,15 @@ for episode in range(EPISODE_NUM):
                 )
                 * old_advantage_tensor,
             )
+        ) - (
+            (
+                torch.exp(original_log_probs - new_log_probs)
+                - original_log_probs
+                - new_log_probs
+                - 1
+            ).sum()
+            * combined_mask_stack
+            / combined_mask_stack.sum()
         )
         policy_loss.backward()
         policy_optimiser.step()
